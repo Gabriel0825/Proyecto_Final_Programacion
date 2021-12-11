1035ed57f9d66402e553a2cbd87e2f1914a24b3f
Gabriel0825 <62782892+Gabriel0825@users.noreply.github.com>
2021-12-10 20:52:40 -0400

    Add files via upload

---
+++ Proyecto Final/src/campo_login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+class campo_login {
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
